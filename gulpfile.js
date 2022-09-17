988b9b6ada7332bbed89900a2d1c335f033e4a7d
Aizzzen <gadamurrr@gmail.com>
2022-09-17 15:18:03 +0300

    modals

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 "use strict";
 
+// пакеты, которые понадобятся в сборке
 const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
+// сюда будем все компилировать
 const dist = "./dist/";
 
+// таски, которые будут помогать собирать проект
+// для отслеживания изменений в файле
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
                 .pipe(gulp.dest(dist))
                 .pipe(browsersync.stream());
 });
 
+// компиляция скриптов в черновом варианте - development
 gulp.task("build-js", () => {
     return gulp.src("./src/js/main.js")
                 .pipe(webpack({
@@ -50,6 +55,8 @@ gulp.task("copy-assets", () => {
                 .on("end", browsersync.reload);
 });
 
+// запускается сервер
+// он серверит файлы в папке dist
 gulp.task("watch", () => {
     browsersync.init({
 		server: "./dist/",
@@ -59,11 +66,13 @@ gulp.task("watch", () => {
     
     gulp.watch("./src/index.html", gulp.parallel("copy-html"));
     gulp.watch("./src/assets/**/*.*", gulp.parallel("copy-assets"));
+    // если в проекте что-то изменится запустится build-js (webpack)
     gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
 });
 
 gulp.task("build", gulp.parallel("copy-html", "copy-assets", "build-js"));
 
+// компиляция скриптов в варианте - production
 gulp.task("build-prod-js", () => {
     return gulp.src("./src/js/main.js")
                 .pipe(webpack({
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import "./slider";
+import modals from './modules/modals';
+
+// скрипты будут выполняться тогда когда DOM структура уже готова
+window.addEventListener("DOMContentLoaded", () => {
+    modals();
+})
+
+console.log("1");
